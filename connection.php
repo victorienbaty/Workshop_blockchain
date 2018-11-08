e743cb686dd53cd114967e54b3d1e9ae966c9ba1
victorienbaty <35293783+victorienbaty@users.noreply.github.com>
2018-11-08 13:45:45 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,5 @@
 <?php
-$dsn = 'mysql:host=51.158.68.31;dbname=worshop_blockchain'; 
-$username = 'root';
-$password = 'Epsi2017!';
+$dsn = 'mysql:host=51.158.68.31;dbname=worshop_blockchain;username="root";password="Epsi2017!"'; 
 $options = array(PDO::MYSQL_ATTR_INIT_COMMAND=> 'SET NAMES utf8');
 
 $conn = new PDO($dsn, $username, $password,$options);
