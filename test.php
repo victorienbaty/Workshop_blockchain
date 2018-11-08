573e8a2d1c8284cb3135d5f61bceae5959ba05c3
victorienbaty <35293783+victorienbaty@users.noreply.github.com>
2018-11-08 13:39:37 +0100

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-include ("config.php");
-
-$row="";
-$db=connect();
-$sql = "SELECT id FROM login WHERE login = 'Vito'";
-foreach($db->query($sql) as $row);
-{print_r($row);}
-var_dump($row);
